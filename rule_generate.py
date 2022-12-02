59bca33565340de33cdaa9636c86e72bb7e5470b
shpark30 <seohyeonpark30.gmail.com>
2022-12-02 09:53:16 +0900

    feat(rule_generate): 과제 번호 추출

--- rule_generate.py
+++ rule_generate.py
@@ -1,6 +1,7 @@
 import sqlalchemy as db
 import pandas as pd
 import access_info as info
+from utils import extract_task_id
 
 
 engine = db.create_engine(info.url)
@@ -8,19 +9,37 @@ engine = db.create_engine(info.url)
 rules: pd.DataFrame = pd.read_sql(info.rule_query, engine)
 rules.sort_values(by="group_nm")
 
+
+# 구문 검사 완료한 데이터셋만 필터링
 def get_diagnosed_hist() -> pd.DataFrame:
-        diagnosis_hist: pd.DataFrame = pd.read_sql(info.syntax_query, engine)
-        has_diagnosed_cond: pd.Series = diagnosis_hist["exec_sttus_cd"]=="COMPLETE"
-        diagnosed_hist: pd.DataFrame = diagnosis_hist[has_diagnosed_cond]["dgnss_id"]
-                .drop_duplicates(subset="dgnss_id")
-        return diagnosed_hist.loc[:, ["dgnss_id", "exec_sttus_cd"]]
+    diagnosis_hist: pd.DataFrame = pd.read_sql(info.syntax_query, engine)
+    has_diagnosed_cond: pd.Series = diagnosis_hist["exec_sttus_cd"] == "COMPLETE"
+    diagnosed_hist: pd.DataFrame = diagnosis_hist[has_diagnosed_cond]["dgnss_id"]
+    .drop_duplicates(subset="dgnss_id")
+    return diagnosed_hist.loc[:, ["dgnss_id", "exec_sttus_cd"]]
+
 
 diagnosed_hist: pd.DataFrame = get_diagnosed_hist()
 rules = pd.merge(
-        left=result,
-        right=diagnosed_hist,
-        how="left",
-        on="dgnss_id")
+    left=result,
+    right=diagnosed_hist,
+    how="left",
+    on="dgnss_id")
 
 has_diagnosed_cond = rules["exec_sttus_cd"]
-diagnosed_rules[""]
+diag_datasets = rules[has_diagnosed_cond]
+
+
+# 과제 번호 추출
+def except_extract_task_id(text):
+    try:
+        return extract_task_id(text)
+    except AttributeError as e:
+        print(e)
+        return
+
+
+diag_datasets['group_id'] = diag_datasets['group_nm'].apply(
+    except_extract_task_id)
+diag_datasets['task_id'] = diag_datasets['dgnss_nm'].apply(
+    except_extract_task_id)
